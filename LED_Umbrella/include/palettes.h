d4fdd7a55808e738813f19d71ca66b3db3c64220
Username-cannot-be-blank <danielheskett@gmail.com>
2021-10-17 11:38:00 -0400

    Delete LED_Umbrella directory

--- LED_Umbrella/include/palettes.h
+++
@@ -1,9 +0,0 @@
-#include <FastLED.h>
-#include <vector>
-
-namespace palettes
-{
-std::vector<CRGB> RWB = {CRGB::Red, CRGB::Blue, CRGB::White};
-std::vector<CRGB> greens ={CRGB::Green, CRGB::GreenYellow, CRGB::YellowGreen};
-std::vector<CRGB> christmas = {CRGB::Red, CRGB::DarkGreen, CRGB::DarkRed, CRGB::Green};
-}
